10e9602c3ab7bdf02da8dec5f16730fdb911bee2
Zach-Allen <zach.allen2814@gmail.com>
2021-02-11 21:58:14 -0700

    Added form post logic

--- tech-iq-lms-client/pages/signup.js
+++ tech-iq-lms-client/pages/signup.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import React, { useState } from 'react';
+import axios from 'axios'
+import { Router, useRouter } from 'next/router';
 
 const SignUpStyles = styled.div`
   input[type='text'],
@@ -76,34 +79,92 @@ const SignUpStyles = styled.div`
 `;
 
 export default function SignUp(){
+  //create instance of router
+  const router = useRouter();
+
+  //Create state objects that will hold the user info
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [userType, setUserType] = useState('');
+
+  //function that will send a Post request to the server
+  const createAccount = async () => {
+    
+    try{
+      const res = await axios.post('test',
+      {
+        firstName,
+        lastName,
+        email,
+        birthdate,
+        password,
+        confirmPassword,
+        userType
+      })
+      if(res.status === 200)
+        router.reload();
+    }
+    catch(e)
+    {
+      alert(e)
+    }
+  }
+
   return(
     <SignUpStyles>
       <h1>New Account</h1>
       <div className="logInContainer">
-        <form className="signUpForm">
+        <form 
+          className="signUpForm"
+          onSubmit={e =>{
+            e.preventDefault()
+            createAccount()
+          }}>
           <div className="textBoxBlock">
             <div>First Name</div>
-            <input type="text" />
+            <input 
+              type="text"
+              onChange={e => setFirstName(e.target.value)}                 
+            />
           </div>
           <div className="textBoxBlock">
             <div>Last Name</div>
-            <input type="text" />
+            <input 
+              type="text"
+              onChange={e => setLastName(e.target.value)}                 
+            />
           </div>          
           <div className="textBoxBlock">
             <div>Email</div>
-            <input type="text" />
+            <input 
+              type="text"
+              onChange={e => setEmail(e.target.value)} 
+            />
           </div>
           <div className="textBoxBlock">
             <div>Birthdate</div>
-            <input type="date" />
+            <input 
+              type="date" 
+              onChange={e => setBirthdate(e.target.value)}
+            />
           </div>
           <div className="textBoxBlock">
             <div>Password</div>
-            <input type="password" />
+            <input 
+              type="password" 
+              onChange={e => setPassword(e.target.value)}
+            />
           </div>
           <div className="textBoxBlock">
             <div>Confirm Password</div>
-            <input type="password" />
+            <input 
+              type="password"
+              onChange={e => setConfirmPassword(e.target.value)}  
+            />
           </div>
           <div className="radioBlock">
             <label>Instructor</label>
