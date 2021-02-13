3b1930a4f9b633d5ea270d2af274797a7672af10
WillLantz <willlantz@mail.weber.edu>
2021-02-13 13:32:42 -0700

    Create landing.js
    Created a simple landing page

---
+++ tech-iq-lms-client/pages/landing.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const LandingStyles = styled.div`
+
+    h1{
+        text-align: center;
+    }
+
+`;
+
+
+
+export default function Landing(){
+
+    return(
+        <LandingStyles>
+            <h1>You have successfully logged in.</h1> 
+        </LandingStyles>
+                  
+    )
+    
+}
