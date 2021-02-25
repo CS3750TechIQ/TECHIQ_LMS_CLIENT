7eb896bcadce05878a4673a95b851e23f41fed0e
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-25 13:44:01 -0700

    border radius and some other CSS for course cards

--- tech-iq-lms-client/components/courseCards.js
+++ tech-iq-lms-client/components/courseCards.js
@@ -6,22 +6,20 @@ const CardStyles = styled.div `
 .card {
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
-  width: 210px;
-  height: 350px;
+  width: 200px;
+  height: 340px;
+  border-radius: 5px;
 }
 
 .card:hover {
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 }
 
-.container {
-  padding: 2px 16px;
-}
-
 .courseColor {
-  width: 210px;
-  height: 100px;
+  width: 200px;
+  height: 95px;
   background-color: #072F5F;
+  border-radius: 5px 5px 0px 0px;
 }
 
 .courseName {
@@ -30,6 +28,7 @@ const CardStyles = styled.div `
 
 .courseDesc {
   padding: 10px 8px;
+  height: 105px;
 }
 
 .viewButton {
@@ -67,7 +66,8 @@ export default function CourseCards() {
       <div class="courseColor"></div>
       <div>
         <h3 class="courseName">Course Name</h3>
-        <p class="courseDesc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed lorem dui. Pellentesque eget ante mauris. </p>
+        <p class="courseDesc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed lorem dui. Pellentesque eget ante mauris.
+          </p>
       </div>
       <div class="center" >
         <button class="viewButton courseButton">View</button> 
