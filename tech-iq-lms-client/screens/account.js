c3b04f638df60a74c0795d4293153235b1a438fd
Zach-Allen <30992058+Zach-Allen@users.noreply.github.com>
2021-03-23 20:01:11 -0600

    Update account.js

--- tech-iq-lms-client/screens/account.js
+++ tech-iq-lms-client/screens/account.js
@@ -8,8 +8,37 @@ import axios from "axios"
 import useLocalStorage from "../hooks/useLocalStorage"
 
 const AccountStyles = styled.div`
+    color: grey;
+
+   input[type="text"],
+   textarea {
+    appearance: none;
+    -webkit-appearance: none;
+    color: #95a5a6;
+    font-family: "Helvetica", arial, sans-serif;
+    font-size: 18px;
+    border: 1px solid #ecf0f1;
+    background: white;
+    margin: 6px;
+    padding: 8px;
+    display: inline-block !important;
+    visibility: visible !important;
+  }
+
+  input[type="text"]:focus,
+  textarea:focus {
+    color: #95a5a6;
+    box-shadow: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+  }
+
   h4 {
     margin-bottom: 0;
+  }
+
+  p, label {
+    padding: .25rem;
   }
 
   .accountContainer {
@@ -60,6 +89,12 @@ const AccountStyles = styled.div`
     padding: 2px;
   }
 
+  .editContainer {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+
   .icon {
     margin: 1rem;
   }
@@ -97,6 +132,18 @@ const AccountStyles = styled.div`
   }
 
   .editButton {
+    font-size: 1.2rem;
+    margin: 1rem;
+    width: 125px;
+    padding: 0.5rem;
+    color: white;
+    border: 1px solid #a0a0a0;
+    border-radius: 1rem;
+    background-color: #0a66c2;
+
+    :hover {
+      transform: scale(1.1);
+    }
   }
 `;
 
@@ -114,6 +161,9 @@ export default function Account() {
   const [ userName, setUserName ] = useState(localUserData[0].username)
   const [ firstName, setFirstName ] = useState('')
   const [ lastName, setLastName] = useState('')
+  const [ githubLink, setGithubLink ] = useState('')
+  const [ linkedInLink, setLinkedInLink ] = useState('')
+  const [ twitterLink, setTwitterLink ] = useState('')
   const [profileImage, setProfileImage] = useState(
     "https://lh3.googleusercontent.com/proxy/CQrbom5H7ldcxHPzgrbcedYPJa8q3RESpTdBEPtiWgYD6aX-YEzHUek4M2XoNaCvFA5ZjAoVvu-xnYIBL6_DUkKowzEdlSckP2M"
     );
@@ -124,6 +174,10 @@ export default function Account() {
     setUserPhone(data?.phone ?? '')
     setFirstName(data?.firstName ?? '')
     setLastName(data?.lastName ?? '')
+    setGithubLink(data?.githubLink ?? 'https://github.com/')
+    setLinkedInLink(data?.linkedInLink ?? 'https://www.linkedin.com/')
+    setTwitterLink(data?.twitterLink ?? 'https://twitter.com/')
+    console.log(data)
     return data
   })
     
@@ -147,6 +201,9 @@ export default function Account() {
         phone: userPhone,
         firstName,
         lastName,
+        githubLink: (githubLink !== '' && githubLink !== null ? githubLink : 'https://github.com/'),
+        linkedInLink: (linkedInLink !== '' && linkedInLink !== null ? linkedInLink : 'https://www.linkedin.com/'),
+        twitterLink: (twitterLink !== '' && twitterLink !== null ? twitterLink : 'https://twitter.com/'),
         userName
       }
     )
@@ -172,6 +229,9 @@ export default function Account() {
       setLastName(data.lastName)
       setUserBio(data.bio)
       setUserPhone(data.phone)
+      setGithubLink(data.githubLink)
+      setLinkedInLink(data.linkedInLink)
+      setTwitterLink(data.twitterLink)
     }
   })
 
@@ -201,25 +261,48 @@ export default function Account() {
             isEditing ? 
             (
             <div>
-              <input type="text" defaultValue={firstName} onChange={(e) => {setFirstName(e.target.value)}}/>
-              <input type="text" defaultValue={lastName} onChange={(e) => {setLastName(e.target.value)}}/>
+              <div className="editContainer">
+                <label>First Name: </label>
+                <input type="text" defaultValue={firstName} onChange={(e) => {setFirstName(e.target.value)}}/>
+              </div>
+              <div className="editContainer">
+                <label>Last Name: </label>
+                <input type="text" defaultValue={lastName} onChange={(e) => {setLastName(e.target.value)}}/>
+              </div>
             </div>
             ) :
             <h1>{firstName + " " + lastName}</h1> 
           }
             {              
               !isEditing ?
-              <p>{userPhone}</p> :
               <div>
+                <p>{userPhone}</p>
+                <p>{userName}</p> 
+              </div> :
+              <div className="editContainer">
+                <label>Phone Number: </label>
                 <input type="text" defaultValue={userPhone} onChange={(e) => {setUserPhone(e.target.value)}} />
+                <p>Email: </p>
+                <p>{userName}</p>
               </div>
             }        
-            <p>{userName}</p>
-            <div className="iconContainer">
-              <SocialIcon className="icon" url="https://github.com/" />
-              <SocialIcon className="icon" url="https://www.linkedin.com/" />
-              <SocialIcon className="icon" url="https://twitter.com" />
-            </div>
+            {
+              isEditing ?
+              <div className="editContainer">
+                <label>Github Profile Page URL</label>
+                <input type="text" defaultValue={githubLink} onChange={(e) => {setGithubLink(e.target.value)}} />
+                <label>Indeed Profile Page URL</label>
+                <input type="text" defaultValue={linkedInLink} onChange={(e) => {setLinkedInLink(e.target.value)}} />
+                <label>Twitter Profile Page URL</label>
+                <input type="text" defaultValue={twitterLink} onChange={(e) => {setTwitterLink(e.target.value)}} />
+              </div> :
+              <div className="iconContainer">
+                <SocialIcon className="icon" url={githubLink} />
+                <SocialIcon className="icon" url={linkedInLink} />
+                <SocialIcon className="icon" url={twitterLink} />
+              </div>
+
+            }
           </div>
         </div>
         <h4>Bio</h4>
@@ -232,8 +315,8 @@ export default function Account() {
         </div>
         {
           isEditing ?
-          <button className="editingButton" onClick={() => {updateUserInfoMutation.mutate()}}>Save</button> :
-          <button className="editingButton" onClick={() => {setIsEditing(true)}}>Edit Page</button>
+          <button className="editButton" onClick={() => {updateUserInfoMutation.mutate()}}>Save</button> :
+          <button className="editButton" onClick={() => {setIsEditing(true)}}>Edit Page</button>
         }
       </div>
     </AccountStyles>
