d5ed12a9014af0ffebbc3570dada6dad4bf7af27
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-04-10 04:06:28 -0600

    Submit payment

--- tech-iq-lms-client/screens/tuition.js
+++ tech-iq-lms-client/screens/tuition.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../components/navBar";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import useLocalStorage from "../hooks/useLocalStorage";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import axios from "axios";
 import { useUser } from "../hooks/useUser";
+
 
 const TuitionStyles = styled.div`
   .tuitionContainer {
@@ -51,12 +52,46 @@ const getTuition = async (studentID) => {
 };
 
 export default function tuition() {
-    const [user] = useUser();
-  const tuitionInfoQuery = useQuery(["tuition", user?.studentId], async () => {
-    return await getTuition(user?.studentId);
-  }, {
-      enabled: !!user?.studentId
-  });
+  const [user] = useUser();
+
+  // set state
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardExpMonth, setCardExpMonth] = useState("");
+  const [cardExpYear, setCardExpYear] = useState("");
+  const [cardCVC, setCardCVC] = useState("");
+
+  const tuitionInfoQuery = useQuery(
+    ["tuition", user?.studentId],
+    async () => {
+      return await getTuition(user?.studentId);
+    },
+    {
+      enabled: !!user?.studentId,
+    }
+  );
+
+  // mutation function
+  const submitPaymentMutation = useMutation(
+    async () => {
+      const { data } = await axios.post(
+        "http://localhost:50058/Account/submitPayment",
+        {
+          amount: amount,
+          description: description,
+          cardNumber: cardNumber,
+          cardExpMonth: cardExpMonth,
+          cardExpYear: cardExpYear,
+          cardCVC: cardCVC
+        }
+      );
+      return data;
+    },
+    {
+      // onSuccess ??
+    }
+  );
 
   if (tuitionInfoQuery.isLoading) {
     return "Loading...";
@@ -70,33 +105,82 @@ export default function tuition() {
     <TuitionStyles>
       <Nav />
       <div className="tuitionContainer">
-        <p className="tuitionTotalContainer">Amount owed: {tuitionInfoQuery?.data.tuition}</p>
+        <p className="tuitionTotalContainer">
+          Amount owed: {tuitionInfoQuery?.data.tuition}
+        </p>
         <div className="inputLabelitems">
           <label>Name on card: </label>
-          <input className="inputBox" type="text" />
+          <input
+            className="inputBox"
+            type="text"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
         </div>
         <div className="inputLabelitems">
           <label>Credit card number: </label>
-          <input className="inputBox" type="text" />
+          <input
+            className="inputBox"
+            type="text"
+            onChange={(e) => {
+              setCardNumber(e.target.value);
+            }}
+          />
         </div>
         <div className="inputLabelitems">
-          <label>Expiration date: </label>
-          <input className="inputBox" type="date" />
+          <label>Expiration Month: </label>
+          <input
+            className="inputBox"
+            type="date"
+            onChange={(e) => {
+              setCardExpMonth(e.target.value);
+            }}
+          />
+        </div>
+        <div className="inputLabelitems">
+          <label>Expiration Year: </label>
+          <input
+            className="inputBox"
+            type="date"
+            onChange={(e) => {
+              setCardExpYear(e.target.value);
+            }}
+          />
         </div>
         <div className="inputLabelitems">
           <label>CVC Number: </label>
-          <input className="inputBox" type="text" />
+          <input
+            className="inputBox"
+            type="text"
+            onChange={(e) => {
+              setCardCVC(e.target.value);
+            }}
+          />
         </div>
         <div className="inputLabelitems">
           <label>Payment amount: </label>
-          <input className="inputBox" type="text" />
+          <input
+            className="inputBox"
+            type="text"
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+          />
         </div>
         <div className="inputLabelitems">
           <label>Memo: </label>
           <input className="inputBox" type="text" />
         </div>
         <div className="paymentButton">
-          <button>Submit payment</button>
+          <button
+            className="submitButton"
+            onClick={() => {
+              submitPaymentMutation.mutate();
+            }}
+          >
+            Submit payment
+          </button>
         </div>
       </div>
     </TuitionStyles>
