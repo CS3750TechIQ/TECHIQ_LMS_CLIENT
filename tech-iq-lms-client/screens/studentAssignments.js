0ac8d6816f4fa4f7b9875c080820468445999199
Heberto Rodriguez <hebertorodriguez@mail.weber.edu>
2021-04-19 00:13:30 -0600

    assignmentsubmit

--- tech-iq-lms-client/screens/studentAssignments.js
+++ tech-iq-lms-client/screens/studentAssignments.js
@@ -54,7 +54,38 @@ const StudentAssignmentsStyles = styled.div`
   }
 `;
 
-const AssignmentSubmissionStyles = styled.div``;
+const AssignmentSubmissionStyles = styled.div`
+   .subTitle{
+       font-weight: 999;
+   }
+   .dataTypeContainer{
+        display: flex;
+        flex-direction: row;
+   }
+   .dataFields{
+       margin-left: auto;
+       margin-right: auto;
+   }
+   .subBody{
+       margin: 5%;
+   }
+   .buto{
+       background-color: #072f5f;
+       border-radius: 4px;
+       box-shadow: 1px 1px 5px #141a18;
+       color: #ffffff;
+
+       :active {
+         background-color: #f3f2ee;
+         box-shadow: 0 3px #666;
+         transform: translateY(2px);
+       }
+   }
+   .butoContainer{
+        text-align: center;
+   }
+
+`;
 
 const fetchAllAssignments = async (courseNumber) => {
   return await axios
@@ -76,6 +107,8 @@ const StudentAssignments = () => {
   const [courseNumber, setCourseNumber] = useState(query.get("courseNumber"));
   const [user, setUser] = useUser();
   const [viewSubmitPage, setViewSubmitPage] = useState(false);
+  const [assignment, setAssignment] = useState();
+
   // function submitAssignment(value) {
   //     debugger;
   //     <RegisterCourses/>
@@ -95,8 +128,11 @@ const StudentAssignments = () => {
         <AssignmentSubmission
           viewSubmitPage={viewSubmitPage}
           setViewSubmitPage={setViewSubmitPage}
+          assignment={assignment}
+          setAssignment={setAssignment}
         />
-      ) : (
+      ) : 
+      (
         <div>
           <Nav />
           <div className="addAssignmentContainer">
@@ -120,6 +156,10 @@ const StudentAssignments = () => {
                         assignment_desc={p.assignment_desc}
                         due_date={p.due_date}
                         max_points={p.max_points}
+                        assignmentID={p.assignmentID}
+                        submission_type={p.submission_type}
+                        setAssignment={setAssignment}
+                        courseNumber={courseNumber}
                       />
                     ))
                   ) : (
@@ -137,7 +177,7 @@ const StudentAssignments = () => {
 
 function Assignment(props) {
   return (
-    <tr>
+    <tr key={props}>
       <td>{props.assignment_title}</td>
       <td>{props.assignment_desc}</td>
       <td> {Moment(props.due_date).format("lll")}</td>
@@ -146,7 +186,8 @@ function Assignment(props) {
         <button
           type="button"
           onClick={() => {
-            props.setViewSubmitPage(true);
+            props.setViewSubmitPage(true)
+            props.setAssignment(props)
           }}
         >
           Submit
@@ -157,19 +198,90 @@ function Assignment(props) {
 }
 
 function AssignmentSubmission(props) {
-  return (
+    const [courseNumber] = useState();
+    const [text, setText] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [user, setUser] = useUser();
+
+    const fileChanged = (e) =>  {
+        const reader = new FileReader();
+        reader.onload = () => {
+        setSelectedFile(reader.result);
+        };
+        reader.readAsText(e.target.files[0]);
+    }
+
+    const assignment = props.assignment
+    const submitAssignment = useMutation(
+        async () => {
+          const data = await axios.put(
+            "http://localhost:50058/Account/SubmitAssignment",
+            {
+              course_number: assignment.courseNumber,
+              AssignmentID: assignment.assignmentID,
+              studentId: user?.studentId,
+              submission_file: selectedFile.name,
+              submission_date: new Date()
+            }
+          )
+          return data
+        },
+        {
+          onSuccess: (data) => {
+            alert("Assignment successfully submitted.");
+          },
+          onError: (err) => {
+            alert("Error submitting assignment.");
+          },
+        }
+      );
+
+    return (
     <AssignmentSubmissionStyles>
       <Nav />
-      <div>Text</div>
-      <a
-        href="#"
-        onClick={() => {
-          props.setViewSubmitPage(false);
-        }}
-      >
-        Go Back to Assignments
-      </a>
+      <div className="subBody">
+        <h3>{assignment.assignment_title}</h3>
+        <hr/>
+        <div className="dataTypeContainer">
+            <div className="dataFields">
+                <label className="subTitle">Due </label>
+                {Moment(assignment.due_date).format("lll")}
+            </div>
+            <span><div className="dataFields">
+            <label className="subTitle">Submitting </label> 
+                {assignment.submission_type}
+            </div></span>
+            <div class="dataFields">
+                <label className="subTitle">Points </label>  
+                {assignment.max_points}
+            </div>
+        </div>
+        <hr/>
+        <div>
+            {assignment.assignment_desc}
+        </div>
+        <hr/>
+        <div class="dataTypeContainer">
+        <div><label className="subTitle">Submit File </label></div>
+            <input
+            type="file"
+            accept="*"
+            onChange={(e) => setSelectedFile(e.target.files[0])}
+            />
+
+            <div><label className="subTitle">Enter text </label></div>
+            <textarea
+            className="txtBox"
+            type="text"
+            value={text}
+            onChange={fileChanged}
+            ></textarea>
+        </div>
+
+        <div class="butoContainer"><button className="buto" onClick={() => {submitAssignment.mutate();}}>Submit</button></div>
+        <a href="#" onClick={() => {props.setViewSubmitPage(false)}}>Go Back to Assignments</a>
+        </div>
     </AssignmentSubmissionStyles>
-  );
+  );  
 }
 export default StudentAssignments;
